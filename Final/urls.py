eb2604e0355eb6c9b681769ffa3d242a1e544b90
mamun700263 <mamun700263@gmail.com>
2024-09-03 09:08:54 +0600

    ignored media

--- Final/urls.py
+++ Final/urls.py
@@ -24,4 +24,6 @@ urlpatterns = [
     path('accounts/',include('accounts.urls')),
     path('review/',include('reviews.urls')),
     path('skill/',include('skill.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
